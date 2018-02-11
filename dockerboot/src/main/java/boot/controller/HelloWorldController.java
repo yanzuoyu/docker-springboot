bfd67be56b8511abb18ca6b1a9f93ea545d0a51e
yzy <yzy@enerbos.com>
2018-02-11 16:44:27 +0800

    helloworld 测试

---
+++ dockerboot/src/main/java/boot/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Greetings from Spring Boot!";
+    }
+}
